78f7b93eaa0bbb1150fabf5aac1fd2a39e3df36c
carlosvinicios <carlosvinicios@id.uff.br>
2022-11-15 13:11:23 -0300

    Mapeamento entre Item e Produto criado

--- CRUD-JPA-registro-de-produtos/src/main/java/modelo/Item.java
+++ CRUD-JPA-registro-de-produtos/src/main/java/modelo/Item.java
@@ -4,14 +4,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "itens")
 public class Item {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@OneToOne
+	@JoinColumn(name = "id_assento")
 	private Produto produto;
 	
 	private Integer quantidade;
